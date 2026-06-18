d693fe15ac22dfe40c891eb0d8faa34de3badd73
BoodyWin <boodywin.official@gmail.com>
2026-06-18 10:45:17 +0300

    nin-math: Added absolute

--- src/math/math.cpp
+++ src/math/math.cpp
@@ -17,7 +17,20 @@ struct MathCosineFn : NinCallable {
     Value call(std::vector<Value> args) override {
         if (!std::holds_alternative<double>(args[0]))
             throw std::runtime_error("cos: Invalid input value");
+
         return cos(std::get<double>(args[0]));
+    }
+};
+
+struct MathAbsFn : NinCallable {
+    int arity() override { return 1; }
+    std::string name() override { return "abs"; }
+    Value call(std::vector<Value> args) override {
+        if (!std::holds_alternative<double>(args[0]))
+            throw std::runtime_error("abs: Invalid input value");
+
+        double x = std::get<double>(args[0]);
+        return x < 0 ? -x : x;
     }
 };
 
@@ -26,5 +39,6 @@ const Value MathPiValue = M_PI;
 extern "C" void carrot_module_init(std::unordered_map<std::string, Value> *out){
     (*out)["sin"] = std::make_shared<MathSineFn>();
     (*out)["cos"] = std::make_shared<MathCosineFn>();
+    (*out)["abs"] = std::make_shared<MathAbsFn>();
     (*out)["pi"] = MathPiValue;
 }
