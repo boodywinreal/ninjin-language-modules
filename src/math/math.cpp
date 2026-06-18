0f530a60638404cc779f39b24bc0d61420df0ccb
BoodyWin <boodywin.official@gmail.com>
2026-06-18 10:50:32 +0300

    nin-math: Added tan

--- src/math/math.cpp
+++ src/math/math.cpp
@@ -6,7 +6,7 @@ struct MathSineFn : NinCallable {
     std::string name() override { return "sin"; }
     Value call(std::vector<Value> args) override {
         if (!std::holds_alternative<double>(args[0]))
-            throw std::runtime_error("");
+            throw std::runtime_error("sin: Invalid input value");
         return sin(std::get<double>(args[0]));
     }
 };
@@ -19,6 +19,17 @@ struct MathCosineFn : NinCallable {
             throw std::runtime_error("cos: Invalid input value");
 
         return cos(std::get<double>(args[0]));
+    }
+};
+
+struct MathTanFn : NinCallable {
+    int arity() override { return 1; }
+    std::string name() override { return "tan"; }
+    Value call(std::vector<Value> args) override {
+        if (!std::holds_alternative<double>(args[0]))
+            throw std::runtime_error("tan: Invalid input value");
+
+        return tan(std::get<double>(args[0]));
     }
 };
 
@@ -39,6 +50,7 @@ const Value MathPiValue = M_PI;
 extern "C" void carrot_module_init(std::unordered_map<std::string, Value> *out){
     (*out)["sin"] = std::make_shared<MathSineFn>();
     (*out)["cos"] = std::make_shared<MathCosineFn>();
+    (*out)["tan"] = std::make_shared<MathTanFn>();
     (*out)["abs"] = std::make_shared<MathAbsFn>();
     (*out)["pi"] = MathPiValue;
 }
